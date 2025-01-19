d11a882b0fd4b3f4035437f194ddf76591fadd5a
cheeseballDev <cheeseballbusinessemail@gmail.com>
2025-01-19 14:24:49 +0800

    Added a messaging feature for the bot (WIP)

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from dotenv import load_dotenv
 
 class Client(discord.Client):
     async def on_ready(self):
-        print('Logged on as{self.user}!')
+        print(f'Logged on as {self.user}!')
 
-    async def on_message(message):
-        print(f'Message from {message.author}: {message.contnet}')
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+        
+        await message.channel.send(message.content)
 
 def main():
     intents = discord.Intents.default()
