0862bbcad2d5c7ef881e917a563715ac7b3b4c29
cheeseballDev <cheeseballbusinessemail@gmail.com>
2025-02-03 22:21:59 +0800

    Added run_bot function and variables for music

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+import yt_dlp
 
 # TO TELL IF BOT RAN SUCCESSFULLY
 class Client(discord.Client):
@@ -12,15 +13,34 @@ class Client(discord.Client):
         if (not message.author.bot and message.content == 'hi'):
             await message.channel.send('hi')
 
+    
+
+
 ## STARTING THE BOT
 def main():
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
     intents = discord.Intents.default()
     intents.message_content = True
-    load_dotenv()
-
-    TOKEN = os.getenv('DISCORD_TOKEN')
-    client = Client(intents=intents)
+    client = discord.Client(intents)
     client.run(TOKEN)
+
+    run_bot(client)
+
+def run_bot(client):
+    voice_clients = {}
+    yt_dl_options = {"format": "bestaudio/best"}
+    ytdl = yt_dlp.YoutubeDL(yt_dl_options)
+
+    ffmpeg = {'options': '-vn'}
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now playing music')
+
+    @client.event
+    async def on_message(message):
+        pass
 
 if __name__ == "__main__":
     main()
