bcb89117b9f207d6bd2f95af7ef1605a29eb3537
cheeseballDev <cheeseballbusinessemail@gmail.com>
2025-02-05 21:44:18 +0800

    Added a play music feature

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 import os
 from dotenv import load_dotenv
@@ -7,13 +8,39 @@ import yt_dlp
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+        print(f'{self.user} is now playing music')
 
     async def on_message(self, message):
+        voice_clients = {}
+        yt_dl_options = {"format": "bestaudio/best"}
+        ytdl = yt_dlp.YoutubeDL(yt_dl_options)
+
+        ffmpeg_options = {'options': '-vn'}
+
+        if message.content.startswith("?play"):
+            try:
+                voice_client = await message.author.voice.channel.connect()
+                voice_clients[voice_client.guild.id] = voice_client
+            except Exception as e:
+                print(e)
+
+            try:
+                url = message.content.split()[1]
+
+                loop = asyncio.get_event_loop()
+                data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+
+                song = data['url']
+                player = discord.FFmpegPCMAudio(song, **ffmpeg_options)
+
+                voice_clients[message.guild.id].play(player)
+
+            except Exception as e:
+                print(e)
+
         print(f'Message from {message.author}: {message.content}')
         if (not message.author.bot and message.content == 'hi'):
             await message.channel.send('hi')
-
-    
 
 
 ## STARTING THE BOT
@@ -22,25 +49,9 @@ def main():
     TOKEN = os.getenv('DISCORD_TOKEN')
     intents = discord.Intents.default()
     intents.message_content = True
-    client = discord.Client(intents)
+    client = Client(intents=intents)
     client.run(TOKEN)
 
-    run_bot(client)
-
-def run_bot(client):
-    voice_clients = {}
-    yt_dl_options = {"format": "bestaudio/best"}
-    ytdl = yt_dlp.YoutubeDL(yt_dl_options)
-
-    ffmpeg = {'options': '-vn'}
-
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is now playing music')
-
-    @client.event
-    async def on_message(message):
-        pass
 
 if __name__ == "__main__":
     main()
